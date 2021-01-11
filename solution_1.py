6244ea68b2331102d0376fa24b44d15096359372
Abhijit Mamarde <abhijit.mamarde@e-zest.in>
2021-01-11 09:34:20 +0530

    added solutions

---
+++ solution_1.py
@@ -0,0 +1,59 @@
+# QUESTION ID: "EZSTIQ0001"
+
+
+def get_words(input_str):
+    words = [x.strip() for x in input_str.split(" ") if x.strip()]
+    return words
+
+
+def get_swapped_words(words):
+    swapped_words = []
+    for w in words:
+        swapped_word = []
+        for i in range(0, len(w), 2):
+            if (i + 1) < len(w):
+                if str.isalpha(w[i + 1]) and str.isalpha(w[i]):
+                    swapped_word.append(w[i + 1])
+                    swapped_word.append(w[i])
+                else:
+                    swapped_word.append(w[i])
+                    swapped_word.append(w[i + 1])
+            else:
+                swapped_word.append(w[i])
+            
+        swapped_words.append("".join(swapped_word))
+    return swapped_words
+
+
+def swap_chars(input_str):
+    # TODO: implement the real logic here!!!
+    # raise NotImplementedError("TODO: implement the real logic here!!!")
+    words = get_words(input_str)
+    swapped_words = get_swapped_words(words)
+    return " ".join(swapped_words)
+
+
+def test_swap_chars():
+    assert swap_chars("Hello World") == "eHllo oWlrd"
+    assert swap_chars("Hello   World") == "eHllo oWlrd"
+    assert swap_chars("Hello +-  World") == "eHllo +- oWlrd"
+    assert swap_chars("Hello +-ab  World") == "eHllo +-ba oWlrd"
+    assert swap_chars("This is    a   sample Test string !?!") == "hTsi si a aspmel eTts tsirgn !?!"
+    assert swap_chars("abc") == "bac"
+    assert swap_chars("abc ") == "bac"
+    assert swap_chars("abc.") == "bac."
+    assert swap_chars("I will be back!") == "I iwll eb abkc!"
+    x = swap_chars("""The 14 English Punctuation Marks With Examples
+By Ali Hale
+Punctuation-marks
+
+Do you know how to use punctuation marks correctly in English? While some might seem straightforward, you may come across punctuation marks that you’re unsure about … so this post is designed to serve as a handy reference.""")
+    assert x == """hTe 14 nElgsih uPcnuttaoin aMkrs iWht xEmalpse
+By lAi aHel
+Pnutcauitno-mrask
+
+oD oyu nkwo ohw ot sue upcnuttaoin amkrs ocrrcelty ni nElgsih? hWlie osem imhgt esme tsargithofwrrad, oyu amy ocem caorss upcnuttaoin amkrs htta oyu’er nuuser bauot … os htsi opts si edisngde ot esvre sa a ahdny erefercne."""
+
+    
+if __name__ == "__main__":
+    test_swap_chars()
---
+++ solution_2.py
@@ -0,0 +1,63 @@
+# QUESTION ID: "EZSTIQ0002"
+
+
+def get_words(input_str):
+    words = [x.strip() for x in input_str.split(" ") if x.strip()]
+    return words
+
+
+def get_swapped_words(words):
+    swapped_words = []
+    for w in words:
+        swapped_word = [w[0]]
+        for i in range(1, len(w) - 1, 2):
+            if (i + 1) < len(w) - 1:
+                if str.isalpha(w[i + 1]) and str.isalpha(w[i]):
+                    swapped_word.append(w[i + 1])
+                    swapped_word.append(w[i])
+                else:
+                    swapped_word.append(w[i])
+                    swapped_word.append(w[i + 1])
+            else:
+                swapped_word.append(w[i])
+
+        if len(w) > 1:
+            swapped_word.append(w[-1])
+        swapped_words.append("".join(swapped_word))
+    return swapped_words
+
+
+def swap_chars(input_str):
+    # TODO: implement the real logic here!!!
+    # raise NotImplementedError("TODO: implement the real logic here!!!")
+    words = get_words(input_str)
+    swapped_words = get_swapped_words(words)
+    return " ".join(swapped_words)
+
+
+def test_swap_chars():
+    assert swap_chars("Hello World") == "Hlelo Wrold"
+    assert swap_chars("Hello   World") == "Hlelo Wrold"
+    assert swap_chars("Hello +-  World") == "Hlelo +- Wrold"
+    assert swap_chars("Hello +-ab  World") == "Hlelo +-ab Wrold"
+    assert swap_chars("Hello +-ab-  World") == "Hlelo +-ab- Wrold"
+    assert swap_chars("Hello +ab-  World") == "Hlelo +ba- Wrold"
+    assert swap_chars("Hello +-+ab-+-  World") == "Hlelo +-+ba-+- Wrold"
+    assert swap_chars("abc") == "abc"
+    assert swap_chars("abcd") == "acbd"
+    assert swap_chars("abcd ") == "acbd"
+    assert swap_chars("I will be back!?!") == "I wlil be bcak!?!"
+    x = """The 14 English Punctuation Marks With Examples
+By Ali Hale
+Punctuation-marks
+
+Do you know how to use punctuation marks correctly in English? While some might seem straightforward, you may come across punctuation marks that you’re unsure about … so this post is designed to serve as a handy reference."""
+    assert swap_chars(x) == """The 14 Egnilsh Pnutcauiton Mraks Wtih Eaxpmels
+By Ali Hlae
+uPcnuttaoin-amkrs
+
+Do you konw how to use pnutcauiton mraks croertcly in Egnilhs? Wihle smoe mgiht seem srtiahgftroawdr, you may cmoe arcsos pnutcauiton mraks taht yuo’re usnrue aobut … so tihs psot is dsegiend to sreve as a hnady rfereneec."""
+    
+
+if __name__ == "__main__":
+    test_swap_chars()
